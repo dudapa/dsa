6fd3254cc251392cebc857a6ff93b7919113e7a0
Patrik Duda <patrikduda94@gmail.com>
2021-12-27 09:45:59 +0100

    Add a Breadth First Search to the Binary Search Tree.

--- dsa/bst.py
+++ dsa/bst.py
@@ -42,14 +42,30 @@ class BinarySearchTree:
         while current_node.left is not None:
             current_node = current_node.left
         return current_node.value
-         
-my_tree = BinarySearchTree()
-my_tree.insert(2)
-my_tree.insert(3)
-my_tree.insert(1)
+    
+    def BFS(self):
+        current_node = self.root
+        results = []
+        queue = []
+        queue.append(current_node)
 
-print(my_tree.root.value)
-print(my_tree.root.left.value)
-print(my_tree.root.right.value)
-print(my_tree.contains(44))
-print(my_tree.min_value_node(my_tree.root))   
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)    
+            if current_node.right is not None:
+                queue.append(current_node.right)
+        return results    
+
+my_tree = BinarySearchTree()
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
+
+result = my_tree.BFS()
+print(result)
