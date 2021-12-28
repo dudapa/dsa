2185763263b1c98b3c6e1e26197b2561110abe06
Patrik Duda <patrikduda94@gmail.com>
2021-12-28 12:11:45 +0100

    Add a dfs_post_order method to the BST.

--- dsa/bst.py
+++ dsa/bst.py
@@ -69,6 +69,20 @@ class BinarySearchTree:
         traverse(self.root)
         return results
 
+    def dfs_post_order(self):
+        results = []
+
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+        
+        traverse(self.root)
+
+        return results
+        
 
 my_tree = BinarySearchTree()
 my_tree.insert(47)
@@ -79,5 +93,5 @@ my_tree.insert(27)
 my_tree.insert(52)
 my_tree.insert(82)
 
-result = my_tree.dfs_pre_order()
+result = my_tree.dfs_post_order()
 print(result)
