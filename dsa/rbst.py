e8d9a77eaa1892170d7d41934a2d48d0229137a5
Patrik Duda <patrikduda94@gmail.com>
2024-04-29 07:51:24 +0200

    Add delete method and its helper function

--- dsa/rbst.py
+++ dsa/rbst.py
@@ -9,7 +9,7 @@ class RecursiveBinarySearchTree:
     def __init__(self):
         self.root = None
     
-    def __r_constains(self, current_node, value):
+    def __r_constains(self, current_node, value):  # We use '__' to prevent user to use this method directly
         if current_node == None:
             return False
         
@@ -20,7 +20,7 @@ class RecursiveBinarySearchTree:
         if value > current_node.value:
             return self.__r_contains(current_node.right, value)
 
-    def r_contains(self, value):
+    def r_contains(self, value): # This method is called by user
         return self.__r_constains(self.root, value)
 
     def __r_insert(self, current_node, value):
@@ -37,16 +37,45 @@ class RecursiveBinarySearchTree:
             self.root = Node(value)
         self.__r_insert(self.root, value)
 
+    def min_value(self, current_node):
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node.value
+
     def __delete_node(self, current_node, value):
         if current_node == None:
             return None
         if value  < current_node.value:
             current_node.left = self.__delete_node(current_node.left, value)
-        
+        if value > current_node.value:
+            current_node.right = self.__delete_node(current_node.right, value)
+        else:
+            if current_node.left == None and current_node.right == None:
+                return None
+            elif current_node.left == None:
+                current_node = current_node.right
+            elif current_node.right == None:
+                current_node = current_node.left
+            else:
+                sub_tree_min = self.min_value(current_node.right)
+                current_node.value = sub_tree_min
+                current_node.right = self.__delete_node(current_node.right, sub_tree_min)
         return current_node
 
     def delete_node(self, value):
         self.root = self.__delete_node(self.root, value)
 
 
-        
+
+my_tree = RecursiveBinarySearchTree()
+my_tree.r_insert(47)
+my_tree.r_insert(21)
+my_tree.r_insert(76)
+my_tree.r_insert(18)
+my_tree.r_insert(27)
+my_tree.r_insert(52)
+my_tree.r_insert(82)
+
+
+print(my_tree.min_value(my_tree.root))
+print(my_tree.min_value(my_tree.root.right))
