006307cdca7c213821b630fb05630f9f154a8e0c
Patrik Duda <patrikduda94@gmail.com>
2024-04-29 07:49:45 +0200

    Add comments

--- dsa/bst.py
+++ dsa/bst.py
@@ -43,7 +43,7 @@ class BinarySearchTree:
             current_node = current_node.left
         return current_node.value
     
-    def BFS(self):
+    def BFS(self): # Breadth First Search
         current_node = self.root
         results = []
         queue = []
@@ -58,7 +58,7 @@ class BinarySearchTree:
                 queue.append(current_node.right)
         return results    
 
-    def dfs_pre_order(self):
+    def dfs_pre_order(self): # Depth First Search (preorder)
         results = []
         def traverse(current_node):
             results.append(current_node.value)
@@ -69,7 +69,7 @@ class BinarySearchTree:
         traverse(self.root)
         return results
 
-    def dfs_post_order(self):
+    def dfs_post_order(self): # Dept First Search (postorder)
         results = []
 
         def traverse(current_node):
@@ -83,7 +83,7 @@ class BinarySearchTree:
 
         return results
 
-    def dfs_in_order(self):
+    def dfs_in_order(self): # Depth First Search (inorder)
         results = []
 
         def traverse(current_node):
